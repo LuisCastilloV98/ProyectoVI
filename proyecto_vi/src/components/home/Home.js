d5b9941d171bd5ffa72259aec829c37a34a97adb
Luis José Castillo Valverde <lui19982011@gmail.com>
2020-06-15 17:53:27 -0600

    Presentaciones
    Integracion de componente de Slides

---
+++ proyecto_vi/src/components/home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Home extends React.Component{
+
+    render(){
+        return(
+            <section>
+            <div>
+              <h2>Visualización de informacion</h2>
+              <p>Factores determinantes en la aprobación de bachillerato costarricense</p>
+              <h4>Luis Jose Castillo, Ignacio Murillo</h4>
+            </div>
+            </section>
+        );
+    }
+}
+
+export default Home;
---
+++ proyecto_vi/src/components/jerarquia/Jerarquia.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Jerarquia extends React.Component{
+
+    render(){
+        return(
+            <section>
+                <h2>
+                    Example
+                </h2>
+            </section>
+        );
+    }
+}
+
+export default Jerarquia;
