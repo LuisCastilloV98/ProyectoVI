fe5db48fe03f8b24a4badf081a0bd038410de2ab
Ignacio Murillo <ignaciomurillo97@gmail.com>
2020-06-28 09:52:51 -0600

    reference area select box

--- proyecto_vi/src/components/small_multiples/chart.js
+++ proyecto_vi/src/components/small_multiples/chart.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import {ScatterChart, CartesianGrid, XAxis, YAxis, Tooltip, Scatter} from 'recharts'
+import {ScatterChart, CartesianGrid, XAxis, YAxis, Tooltip, Scatter, ReferenceArea} from 'recharts'
 
 class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      areaStart: null,
+      areaEnd: null,
+    }
+  }
+
+  selectData() {
+
+    this.setState({areaEnd: null, areaStart: null});
+  }
+
   render() {
     let zipedData = this.props.xAxis.map((x,i) => {
       let value = {
@@ -18,19 +32,32 @@ class Chart extends React.Component {
 
     return (
       <ScatterChart width={300} height={300}
-                    onMouseDown={(e) => {console.log(e)}}
+                    onMouseDown={(e) => {this.setState({areaStart: [e.xValue, e.yValue]})}}
+                    onMouseMove={(e) => {e!=null &&
+                                         this.state.areaStart != null &&
+                                         this.setState({areaEnd: [e.xValue, e.yValue]})
+                                        }}
+                    onMouseUp={(e) => {this.selectData()}}
                     margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
         <CartesianGrid
           strokeDasharray="3 3" />
         <YAxis dataKey={ this.props.yCategory } name={ this.props.yCategory } tick={{fontSize: 10}} type='number'/>
         <XAxis dataKey={ this.props.xCategory } name={ this.props.xCategory } tick={{fontSize: 10}} type='number'/>
-        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter onClick={this.props.dataClick} name={`${this.props.xCategory}, ${this.props.yCategory}`} data={zipedData} fill="#8884d8" />
         <Scatter
           onClick={this.props.dataClick}
           name={`${this.props.xCategory}, ${this.props.yCategory}`}
           data={zipedSelectedData}
           fill="#88d884" />
+        {
+          (this.state.areaStart != null && this.state.areaEnd != null) ?
+            <ReferenceArea x1={this.state.areaStart[0]}
+              y1={this.state.areaStart[1]}
+              x2={this.state.areaEnd[0]}
+              y2={this.state.areaEnd[1]}
+              strokeOpacity={0.3}
+            /> : <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        }
       </ScatterChart>
     )
   }
